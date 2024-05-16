e0a8519c0101ae3e15f1fae8fc7f277bc963ecc0
nmzaki <49403549+nmzaki@users.noreply.github.com>
2024-05-16 13:03:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Задача 1
 # Вариант 1
 
 figure = int(input('Введите пятизначное число \n'))
@@ -22,6 +23,10 @@ print(int(sub_fig1 + (sub_fig4 * 100) + sub_fig3 + (sub_fig2 / 100) + sub_fig5))
 # new_figure = figure[0] + figure[3] + figure[2] + figure[1] + figure[4]
 # print(new_figure)
 
+
+
+# Задача 2
+
 work_days = int(input('Введите количество дней до ближайшего отпуска \n'))
 weeks = work_days // 7
 if work_days < 5:
@@ -32,3 +37,14 @@ elif work_days > 7 and (work_days - weeks * 7) < 5:
     print(weeks * 2)
 else:
     print((weeks * 2) + (work_days - (weeks * 7) - 5))
+
+# Задача 3
+# length, width, size = (
+#     int(input("Введите длинну: ")),
+#     int(input("Введите ширину: ")),
+#     int(input("Введите размер куска: ")),
+# )
+# if size % length == 0 or size % width == 0:
+#     print(True)
+# else:
+#     print(False)
